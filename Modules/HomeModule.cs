0c4fb505e71062679bb5d10dfdc4dcc02d97b919
Jill Kuchman <pillanoid@gmail.com>
2016-01-23 20:01:59 -0800

    Add category list and category pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -4,29 +4,46 @@ using Nancy.ViewEngines.Razor;
 
 namespace ToDoList
 {
-
     public class HomeModule : NancyModule
     {
         public HomeModule()
         {
             Get["/"] = _ => {
-              return View["index.cshtml"];
+              List<Category> AllCategories = Category.GetAll();
+              return View["index.cshtml", AllCategories];
             };
             Get["/tasks"] = _ => {
               List<Task> AllTasks = Task.GetAll();
               return View["tasks.cshtml", AllTasks];
             };
             Get["/tasks/new"] = _ => {
-              return View["tasks_form.cshtml"];
+              List<Category> AllCategories = Category.GetAll();
+              return View["tasks_form.cshtml", AllCategories];
             };
             Post["/tasks/new"] = _ => {
-              Task newTask = new Task(Request.Form["task-description"], 1);
+              Task newTask = new Task(Request.Form["task-description"], Request.Form["category-id"]);
               newTask.Save();
               return View["success.cshtml"];
             };
             Post["/tasks/delete"] = _ => {
               Task.DeleteAll();
               return View["cleared.cshtml"];
+            };
+            Get["/categories/new"] = _ => {
+              return View["categories_form.cshtml"];
+            };
+            Post["/categories"] = _ => {
+              Category newCategory = new Category(Request.Form["category-name"]);
+              newCategory.Save();
+              return View["success.cshtml"];
+            };
+            Get["categories/{id}"] = parameters => {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              var SelectedCategory = Category.Find(parameters.id);
+              var CategoryTasks = SelectedCategory.GetTasks();
+              model.Add("category", SelectedCategory);
+              model.Add("tasks", AllTasks);
+              return View["/category.cshtml", model];
             };
         }
     }
